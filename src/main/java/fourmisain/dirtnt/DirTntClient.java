ce7d9ff187ef0abb51468d537776fec48efcccbe
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-12-01 05:20:46 +0100

    client.DirTNTClient -> DirTntClient

--- src/main/java/fourmisain/dirtnt/DirTntClient.java
+++ src/main/java/fourmisain/dirtnt/DirTntClient.java
@@ -1,13 +1,13 @@
-package fourmisain.dirtnt.client;
+package fourmisain.dirtnt;
 
-import fourmisain.dirtnt.DirTnt;
+import fourmisain.dirtnt.client.DirtTntEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 @Environment(EnvType.CLIENT)
-public class DirTNTClient implements ClientModInitializer {
+public class DirTntClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		EntityRendererRegistry.register(DirTnt.DIRT_TNT_ENTITY_TYPE, DirtTntEntityRenderer::new);
